ea15f93c46d442589bb2f227ecd951e9b7a6ce6f
karthikeya-arikirevula-ics4u1-02-25 <karthikeya.arikirevula@student.tdsb.on.ca>
2025-02-11 03:02:03 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class PracticeProblem {
 
 	public static void main(String args[]) {
+		Scanner in = new Scanner(System.in);
 		q1();
 		q2();
 		q3();
@@ -18,60 +19,118 @@ public class PracticeProblem {
 	}
 
 	public static void q1() {
-		//Write question 1 code here
-		
+		Scanner in = new Scanner(System.in);
+		System.out.print("Input a number: ");
+		System.out.println(Math.abs(in.nextDouble()));
+		in.nextLine();
 	}
 
 	public static void q2() {
-		//Write question 2 code here
-		
+		Scanner in = new Scanner(System.in);
+		double num;
+		double num2;
+		System.out.print("Input a number: ");
+		num = in.nextDouble();
+		in.nextLine();
+		System.out.print("Input another number: ");
+		num2 = in.nextDouble();
+		in.nextLine();
+		System.out.println(Math.floor(num/num2));
+		System.out.println(Math.ceil(num/num2));
 	}
 
 	public static void q3() {
-		//Write question 3 code here
-		
+		Scanner in = new Scanner(System.in);
+		System.out.print("Input a number: ");
+		System.out.println(Math.round(Math.sqrt(in.nextInt())));
 	}
 
 	public static void q4() {
-		//Write question 4 code here
-		
+		Scanner in = new Scanner(System.in);
+		int num;
+		int num2;
+		System.out.print("Input a number: ");
+		num = in.nextInt();
+		in.nextLine();
+		System.out.print("Input another number: ");
+		num2 = in.nextInt();
+		in.nextLine();
+		System.out.println(Math.pow(num, num2));
 	}
 
 	public static void q5() {
-		//Write question 5 code here
-		
+		Scanner in = new Scanner(System.in);
+		double num;
+		double num2;
+		double num3;
+		System.out.print("Input a number: ");
+		num = in.nextDouble();
+		in.nextLine();
+		System.out.print("Input another number: ");
+		num2 = in.nextDouble();
+		in.nextLine();
+		System.out.print("Input one more number: ");
+		num3 = in.nextDouble();
+		in.nextLine();
+		System.out.println(Math.max(num,Math.max(num2, num3)));
+		System.out.println(Math.min(num,Math.min(num2, num3)));
 	}
 	public static void q6() {
-		//Write question 5 code here
-		
+		Scanner in = new Scanner(System.in);
+		System.out.print("Input a sentence: ");
+		System.out.println(in.nextLine().contains("on"));
 	}
 	public static void q7() {
-		//Write question 5 code here
-		
+		Scanner in = new Scanner(System.in);
+		System.out.print("Input the word mango: ");
+		System.out.println(in.nextLine().equalsIgnoreCase("mango"));
 	}
 	public static void q8() {
-		//Write question 5 code here
-		
+		Scanner in = new Scanner(System.in);
+		String word;
+		String oneword;
+		System.out.print("Input a word: ");
+		word = in.nextLine();
+		System.out.print("Input a letter: ");
+		oneword = in.nextLine();
+		System.out.println(word.indexOf(oneword));
+		System.out.println(word.lastIndexOf(oneword));
 	}
 
 	public static void q9() {
-		//Write question 5 code here
-		
+		Scanner in = new Scanner(System.in);
+		System.out.print("Input a sentence: ");
+		System.out.println("Your sentence is " + in.nextLine().length() + " characters long");
 	}
 	public static void q10() {
-		//Write question 5 code here
-		
+		Scanner in = new Scanner(System.in);
+		String word;
+		String word2;
+		String word3;
+		System.out.print("Input a sentence: ");
+		word = in.nextLine();
+		System.out.print("Input a word to replace: ");
+		word2 = in.nextLine();
+		System.out.print("What word would you like to replace it with: ");
+		word3 = in.nextLine();
+		System.out.println(word.replaceAll(word2, word3));
 	}
 
 	public static void q11() {
-		//Write question 5 code here
-		
+		Scanner in = new Scanner(System.in);
+		String word;
+		System.out.print("Input a sentence: ");
+		word = in.nextLine();
+		System.out.println(word.toUpperCase().trim());
+		System.out.println(word.toLowerCase().trim());
 	}
 
 	public static void q12() {
-		//Write question 5 code here
-		
+		Scanner in = new Scanner(System.in);
+		String word;
+		System.out.print("Input a word: ");
+		word = in.nextLine();
+		System.out.println(word.substring(0,4));
+		System.out.println(word.substring(word.length() - 4));
 	}
-	
-
 }
